8b9be13356788a9cc0a4085d35572307c5faa744
mustafa <mustafadervisoglu16@gmail.com>
2022-10-21 18:53:19 +0300

    added xAmount function.  modified resultBet and transfer functions

--- contracts/Bet.sol
+++ contracts/Bet.sol
@@ -4,33 +4,40 @@ pragma solidity ^0.8.9;
 contract Bet {
 
     address public owner;
-    uint256 public randomNumber;
-    bool public win = true;
+    uint public randomNumber;
+    bool public win = false;
 
     mapping(address => uint) public bettorsAmounts;
 
-    event Transfer(address indexed _from, address _to, uint amount);
-    event BettorTransfer(address indexed _from, address _to, uint amount);
+    event Lose(address indexed _from, address _to, uint amount);
+    event Win(address indexed _from, address _to, uint amount);
     constructor(){
         owner = msg.sender;
     }
 
     function resultBet(uint bettorNumber) external view returns (bool) {
+        require(bettorNumber < 97, "your number must be greater than 97");
+
         randomNumber = uint8(uint256(keccak256(
-                abi.encodePacked(block.timestamp, block.difficulty))) % 94);
+                abi.encodePacked(block.timestamp, block.difficulty))) % 100);
 
         if (bettorNumber < randomNumber && bettorNumber == randomNumber) {
-            win = false;
+            win = true;
         }
         return win;
     }
 
-    function transfer(address bettor, uint amount) internal returns (bool){
+    function transfer(address bettor, uint amount, uint bettorNumber) internal returns (bool){
+        uint totalAmount = xAmount(bettorNumber, amount);
         if (win) {
-            emit BettorTransfer(msg.sender, bettor, amount);
-        }else{
-            emit Transfer(bettor,msg.sender,amount);
+            emit Win(msg.sender, bettor, amount);
+        } else {
+            emit Lose(bettor, msg.sender, amount);
         }
         return true;
+    }
+
+    function xAmount(uint bettorNumber, uint bettorAmount) internal returns (uint){
+        return ((100 / (bettorNumber - 1)) - ((100 - bettorNumber) / 100)) * bettorAmount;
     }
 }
