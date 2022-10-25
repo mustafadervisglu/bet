60039b352beb0198a6802ff2ea228c21576269ec
mustafa <mustafadervisoglu16@gmail.com>
2022-10-25 18:27:28 +0300

    added modifiers and events(Win,Lose,Received,Withdraw). roll, withdraw, randomNumberGenerator functions added. struct Result added

--- contracts/Bet.sol
+++ contracts/Bet.sol
@@ -1,53 +1,80 @@
-// SPDX-License-Identifier: MIT
-pragma solidity >=0.7.0 <0.9.0;
+// SPDX-License-Identifier: MIT;
+pragma solidity ^0.8.0;
 
 contract Bet {
 
-    address public owner;
+    address payable public owner;
     uint public randomNumber;
+    uint256 public betId;
+    mapping(uint256 => Result) public results;
 
+    struct Result{
+        uint256 id;
+        uint256 bet;
+        uint256 amount;
+        address payable player;
+    }
 
-    mapping(address => uint) public bettorsAmounts;
-
-
-    constructor() payable  {
+    constructor() payable {
         owner = payable(msg.sender);
     }
-    event Lose(address indexed _from, address _to, uint amount);
-    event Win(address indexed _from, address _to, uint amount);
-    //function that calculates bet result;
 
-    function resultBet(uint bettorNumber, uint amount, address payable bettorAddress) payable betRole(bettorNumber) external returns (bool) {
+    event Win(uint256 id, uint256 bet, uint256 randomNumber, uint256 amount, address player, uint256 time);
+    event Lose(uint256 id, uint256 bet, uint256 randomNumber, uint256 amount, address player, uint256 time);
+    event Received(address indexed _from, uint256 _amount);
+    event Withdraw(address indexed _from, address _to, uint256 _amount);
 
-        uint totalAmount = xAmount(bettorNumber, amount);
-        require(address(this).balance > totalAmount, "your balance must be greater than your bet");
-        randomNumber = uint8(uint256(keccak256(
-                abi.encodePacked(block.timestamp, block.difficulty))) % 100);
+    //function that generates random number;
 
-        if (bettorNumber > randomNumber) {
-            (bool sent,) = payable(owner).call{value : totalAmount}("");
-            // bool winBettor = bettorAddress.send(totalAmount) transfer bettor;
-            emit Win(owner, bettorAddress, totalAmount);
-            return sent;
-        } else {
-            (bool sent,) = bettorAddress.call{value : totalAmount}("");
-            emit Lose(bettorAddress, owner, totalAmount);
-            return sent;
+    function randomNumberGenerator() internal returns(uint){
+        randomNumber = uint8(uint256(keccak256(abi.encodePacked(block.timestamp,block.difficulty)))%100);
+        this.resultBet(randomNumber);
+        return randomNumber;
+    }
+
+    function roll(uint bettorNumber)public betRole(bettorNumber) payable returns(bool) {
+        require(msg.sender.balance >= 1 ether, "Error, msg.value must be at least equal to 1 eth");
+        require(msg.value > 0, "Error, msg.value must be greater than 0");
+        betId += 1;
+        results[betId] = Result(betId, bettorNumber ,msg.value, payable(msg.sender));
+        randomNumberGenerator();
+        return true;
+    }
+
+
+    function resultBet(uint random) external payable returns (bool) {
+        uint winAmount = 0;
+        for (uint256 i = betId - 1; i < betId; i++) {
+            if (random < results[i].bet) {
+                // winAmount that calculates how much money the user will earn;
+                winAmount = ((981000 / results[i].bet) * msg.value) / 10000;
+                results[i].player.transfer(winAmount);
+                emit Win(results[i].id, results[i].bet, randomNumber, winAmount,results[i].player, block.timestamp);
+            }
+            emit Lose(results[i].id, results[i].bet, randomNumber, winAmount, results[i].player, block.timestamp);
         }
+        return true;
+    }
 
-        // transfer contract
 
+    function withdraw(uint amount) external onlyOwner payable {
+        require(address(this).balance >= amount, "your amount must be equal contract balance or greater than contract balance");
+        owner.transfer(amount);
+        emit Withdraw(owner, address(this),amount);
+    }
+
+    modifier onlyOwner(){
+        require(msg.sender == owner, "only owner");
+        _;
     }
 
     modifier betRole(uint bettorNumber) {
-        require(bettorNumber < 97, "your number must be greater than 97");
+        require(bettorNumber > 0, "your number must be greater than 0");
+        require(bettorNumber < 97, "your number must be smaller than 97");
         _;
-    }
-    // function that calculates how much money the user will earn;
-    function xAmount(uint bettorNumber, uint bettorAmount) public pure returns (uint){
-        return (((100 / (bettorNumber - 1)) - (100 - bettorNumber) / 100)) * bettorAmount;
     }
 
     receive() external payable {
+        emit Received(msg.sender, msg.value);
     }
 }
