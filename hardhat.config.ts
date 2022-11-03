754246896fb2426aad1fb270f9950addd824ce40
mustafa <mustafadervisoglu16@gmail.com>
2022-11-03 18:43:07 +0300

    modified hardhat.config.ts and deploy.ts. Added verify function for etherscan and ETHERSCAN_API_KEY

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -7,14 +7,16 @@ import "solidity-coverage";
 import "@typechain/hardhat";
 import "@nomiclabs/hardhat-ethers";
 import "@nomiclabs/hardhat-waffle";
+import "@nomiclabs/hardhat-etherscan";
 import * as dotenv from "dotenv";
 
 dotenv.config();
-
 const HARDHAT = process.env.HARDHAT;
 const BNB_RPC_URL = process.env.BNB_RPC_URL;
 const PRIVATE_KEY: any = process.env.PRIVATE_KEY;
 const GOERLI_RPC_URL = process.env.ETH_GOERLI_RPC_URL;
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY;
+
 const config: HardhatUserConfig = {
     defaultNetwork: "hardhat",
     gasReporter: {
@@ -42,6 +44,9 @@ const config: HardhatUserConfig = {
             chainId: 97,
             accounts: [PRIVATE_KEY]
         }
+    },
+    etherscan: {
+        apiKey: ETHERSCAN_API_KEY
     },
     solidity: "0.8.17"
 };
--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,24 +1,42 @@
 //imports
 import {ethers, run, network} from "hardhat";
+import type {BigNumber} from "ethers";
+import {Bet, Bet__factory} from "../typechain-types";
+
+const CONSTRUCTOR_ADDRESS = process.env.CONSTRUCTOR_ADDRESS;
 
 //async main
 async function main() {
-    const Bet = await ethers.getContractFactory("Bet");
+    const Bet: Bet__factory = await ethers.getContractFactory("Bet");
 
     console.log("Deploying Contract...");
-    const bet = await Bet.deploy({value: ethers.utils.parseEther("100")});
+    const bet: Bet = await Bet.deploy({value: ethers.utils.parseEther("0.05")});
     await bet.deployed();
     // hardhat generates public key if you don't give any public key
-    console.log(network.config);
-
+    // console.log(network.config);
+    console.log(`Deployed contract to: ${bet.address}`);
+    // goerli test network chainId number is 5
+    if(network.config.chainId === 5 && process.env.ETHERSCAN_API_KEY) {
+        //wait 5 blocks for run verificaiton
+        await bet.deployTransaction.wait(5);
+        await verify(bet.address, CONSTRUCTOR_ADDRESS);
+    }
 }
 
-async function verify(address: any, arg: any) {
+async function verify(address: string, arg: string | undefined) {
     console.log("verifying contract");
-    await run("verify:verify", {
-        address: address,
-        constructorArguments: arg
-    });
+    try {
+        await run("verify:verify", {
+            address: address,
+            constructorArgumentsAddress: arg
+        });
+    } catch(e: any) {
+        if(e.message.toLocaleLowerCase().includes("already verified")) {
+            console.log("Already Verified!");
+        } else {
+            console.log(e);
+        }
+    }
 }
 
 //main
